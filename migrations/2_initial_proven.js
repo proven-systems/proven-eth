6ceaf8d48a6435ef90e2a097b184cc3b450cb952
Chuck LeDuc Díaz <celeduc@gmail.com>
2017-12-03 16:57:03 +0100

    add deployer.link between contracts because whatever

--- migrations/2_initial_proven.js
+++ migrations/2_initial_proven.js
@@ -4,8 +4,13 @@ var ProvenRegistry = artifacts.require("ProvenRegistry");
 
 module.exports = function(deployer) {
   deployer.deploy(ProvenRegistry).then(function() {
+    deployer.link(ProvenRegistry, Proven);
     return deployer.deploy(Proven, ProvenRegistry.address).then(function() {
-      return deployer.deploy(ProvenDb, ProvenRegistry.address);
+      deployer.link(ProvenRegistry, ProvenDb);
+      return deployer.deploy(ProvenDb, ProvenRegistry.address).then(function() {
+        ProvenRegistry.at(ProvenRegistry.address).setProven(Proven.address);
+        ProvenRegistry.at(ProvenRegistry.address).setDb(ProvenDb.address);
+      });
     })
   })
 };
