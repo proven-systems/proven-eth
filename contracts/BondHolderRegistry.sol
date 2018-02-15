8d55ba3fde1c6299553d39a7c6194670dc04371f
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-02-15 16:31:20 +0100

    remove unused db parameter from BondHolderRegistry

--- contracts/BondHolderRegistry.sol
+++ contracts/BondHolderRegistry.sol
@@ -9,13 +9,14 @@ import "./Ownable.sol";
 contract BondHolderRegistry is Ownable {
 
   address public bondHolder;
-  address public db;
+//  address public db;
 
   function setBondHolder(address _bondHolder) public onlyOwner {
     bondHolder = _bondHolder;
   }
 
-  function setDB(address _db) public onlyOwner {
-    db = _db;
-  }
+  // TODO: break the data structures out into a contract that doesn't need to be updated
+//  function setDB(address _db) public onlyOwner {
+//    db = _db;
+//  }
 }
