7b2520cafb06f747c8a8b57a617ef49aafa94cf4
Chuck LeDuc Díaz <celeduc@gmail.com>
2018-04-07 20:12:54 +0200

    fix camel case required in migrations

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.4.18;
 
 contract Migrations {
   address public owner;
-  uint public last_completed_migration;
+  uint public lastCompletedMigration;
 
   modifier restricted() {
     if (msg.sender == owner) {
@@ -19,11 +19,11 @@ contract Migrations {
   }
 
   function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
+    lastCompletedMigration = completed;
   }
 
   function upgrade(address newAddress) public restricted {
     Migrations upgraded = Migrations(newAddress);
-    upgraded.setCompleted(last_completed_migration);
+    upgraded.setCompleted(lastCompletedMigration);
   }
 }
